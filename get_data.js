219a11a1a4ac93702c83d204df46652de8f43ff3
Caleb Conner <calebc898@gmail.com>
2020-04-29 19:23:02 -0400

    Shifted selectors to jQuery

--- get_data.js
+++ get_data.js
@@ -12,78 +12,70 @@ d3.csv("test.csv", function(error, data) {
     var team2data = data[1];
 
     // stuff for box at top
-    var team1name = document.getElementById("team-one-name");
-	var team1year = document.getElementById("team-one-year");
-    var team1points = document.getElementById("team-one-points");
-    var team1fg = document.getElementById("team-one-fg");
+    $("#team-one-name").text(team1data.Team);
+    $("#team-one-year").text("Season: " + team1data.Season);
+    $("#team-one-points").text("Total Points: " + team1data.PTS);
+    $("#team-one-fg").text("FG%: " + team1data.FG);
 
-    var team2name = document.getElementById("team-two-name");
-    var team2year = document.getElementById("team-two-year");
-    var team2points = document.getElementById("team-two-points");
-    var team2fg = document.getElementById("team-two-fg");
-
-    team1name.textContent = team1data.Team;
-    team1year.textContent = "Season: " + team1data.Season;
-    team1points.textContent = "Total Points: " + team1data.PTS;
-    team1fg.textContent = "FG%: " + team1data.FG;
-
-    team2name.textContent = team2data.Team;
-    team2year.textContent = "Season: " + team2data.Season;
-    team2points.textContent = "Total Points: " + team2data.PTS;
-    team2fg.textContent = "FG%: " + team2data.FG;
-
-    // box above court for displaying stats
-    var statDisplay = document.getElementById("stat-display");
-    var statTeamDisplay = document.getElementById("stat-team-name");
-    
-    // elements of team 1 court (left side)
-    var team1three = document.getElementById("team-one-three");
-    var team1two = document.getElementById("team-one-two");
-    var team1ft = document.getElementById("team-one-ft");
-   	
-   	// elements of team 2 court (right side)
-    var team2three = document.getElementById("team-two-three");
-    var team2two = document.getElementById("team-two-two");
-    var team2ft = document.getElementById("team-two-ft");
+    $("#team-two-name").text(team2data.Team);
+    $("#team-two-year").text("Season: " + team2data.Season);
+    $("#team-two-points").text("Total Points: " + team2data.PTS);
+    $("#team-two-fg").text("FG%: " + team2data.FG);
 
     // default text display
-    statTeamDisplay.textContent = "Hover over an element";
-    statDisplay.textContent = "to see a stat";
+    $("#stat-team-name").text("Hover over an element");
+    $("#stat-display").text("to see a stat");
 
-    // Team 1 hover events
-    team1three.onmouseover = function() {
-    	statTeamDisplay.textContent = team1data.Team;
-    	statDisplay.textContent = team1data.threeP;
-    	console.log(team1data.threeP);
-    }
-    team1two.onmouseover = function() {
-    	statTeamDisplay.textContent = team1data.Team;
-    	statDisplay.textContent = team1data.twoP;
-    	console.log(team1data.twoP);
-    }
-    team1ft.onmouseover = function() {
-    	statTeamDisplay.textContent = team1data.Team;
-    	statDisplay.textContent = team1data.FT;
-    	console.log(team1data.FT);
-    }
+    // D3 format is to put a '+' in front to change an element into a number
+    // to convert to a percentage: ((+element)*100) + "%"
 
-    // Team 2 hover events
-    team2three.onmouseover = function() {
-    	statTeamDisplay.textContent = team2data.Team;
-    	statDisplay.textContent = team2data.threeP;
-    	console.log(team2data.threeP);
-    }
-    team2two.onmouseover = function() {
-    	statTeamDisplay.textContent = team2data.Team;
-    	statDisplay.textContent = team2data.twoP;
-    	console.log(team2data.twoP);
-    }
-    team2ft.onmouseover = function() {
-    	statTeamDisplay.textContent = team2data.Team;
-    	statDisplay.textContent = team2data.FT;
-    	console.log(team2data.FT);
-    }
+    var eventCount = 0;
 
+    // Team 1 hover functions
+    $("#team-one-three")
+    	.mouseover(function() {
+    		eventCount++;
+    		$("#stat-team-name").text(team1data.Team);
+    		$("#stat-display").text("Three-Point: " + ((+team1data.threeP)*100) + "%");
+    		console.log(eventCount+ " Three-Point: " + ((+team1data.threeP)*100) + "%");
+   		});
+    $("#team-one-two")
+    	.mouseover(function() {
+			eventCount++;
+    		$("#stat-team-name").text(team1data.Team);
+    		$("#stat-display").text("Two-Point: " + ((+team1data.twoP)*100) + "%");
+    		console.log(eventCount+" Two-Point: " + ((+team1data.twoP)*100) + "%");
+    	});
+    $("#team-one-ft")
+    	.mouseover(function() {
+    		eventCount++;
+    		$("#stat-team-name").text(team1data.Team);
+    		$("#stat-display").text("Free-Throw: " + ((+team1data.FT)*100) + "%");
+    		console.log(eventCount+" Free-Throw: " + ((+team1data.FT)*100) + "%");
+   		});
 
+    // Team 2 hover functions
+    $("#team-two-three")
+    	.mouseover(function() {
+    		eventCount++;
+       		$("#stat-team-name").text(team2data.Team);
+    		$("#stat-display").text("Three-Point: " + ((+team2data.threeP)*100) + "%");
+    		console.log(eventCount+" Three-Point: " + ((+team2data.threeP)*100) + "%");
+    	});
+
+    $("#team-two-two")
+    	.mouseover(function() {
+    		eventCount++;
+    		$("#stat-team-name").text(team2data.Team);
+    		$("#stat-display").text("Two-Point: " + ((+team2data.twoP)*100) + "%");
+    		console.log(eventCount+" Two-Point: " + ((+team2data.twoP)*100) + "%");
+    	});
+    $("#team-two-ft")
+    	.mouseover(function() {
+    		eventCount++;
+	    	$("#stat-team-name").text(team2data.Team);
+	    	$("#stat-display").text("Free-Throw: " + ((+team2data.FT)*100) + "%");
+	    	console.log(eventCount+" Free-Throw: " + ((+team2data.FT)*100) + "%");
+    	});	
 
 });
