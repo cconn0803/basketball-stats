82755b8fb1ab8a5732bc73823f5fbfad1547a3cb
Caleb Conner <calebc898@gmail.com>
2020-04-29 20:13:23 -0400

    Flags added for hover elements, functioning almost perfectly

--- get_data.js
+++ get_data.js
@@ -29,53 +29,85 @@ d3.csv("test.csv", function(error, data) {
     // D3 format is to put a '+' in front to change an element into a number
     // to convert to a percentage: ((+element)*100) + "%"
 
-    var eventCount = 0;
+    // flags for hover events
+  	var twoFlag1 = false;
+  	var ftFlag1 = false;
+  	var twoFlag2 = false;
+  	var ftFlag2 = false;
+
 
     // Team 1 hover functions
     $("#team-one-three")
     	.mouseover(function() {
-    		eventCount++;
-    		$("#stat-team-name").text(team1data.Team);
-    		$("#stat-display").text("Three-Point: " + ((+team1data.threeP)*100) + "%");
-    		console.log(eventCount+ " Three-Point: " + ((+team1data.threeP)*100) + "%");
+    		if( !twoFlag1 ){
+    			$("#stat-team-name").text(team1data.Team);
+    			$("#stat-display").text("Three-Point: " + ((+team1data.threeP)*100) + "%");
+    		}
+   		})
+   		.mouseleave(function() {
+   			// Back to default
+   			$("#stat-team-name").text("Hover over an element");
+   			$("#stat-display").text("to see a stat");
    		});
+
     $("#team-one-two")
     	.mouseover(function() {
-			eventCount++;
-    		$("#stat-team-name").text(team1data.Team);
-    		$("#stat-display").text("Two-Point: " + ((+team1data.twoP)*100) + "%");
-    		console.log(eventCount+" Two-Point: " + ((+team1data.twoP)*100) + "%");
-    	});
+    		if( !ftFlag1 ){
+    			twoFlag1 = true;
+	    		$("#stat-team-name").text(team1data.Team);
+	    		$("#stat-display").text("Two-Point: " + ((+team1data.twoP)*100) + "%");
+	    	}
+    	})
+    	.mouseleave(function() {
+    		twoFlag1 = false;
+   		});
+
     $("#team-one-ft")
     	.mouseover(function() {
-    		eventCount++;
-    		$("#stat-team-name").text(team1data.Team);
-    		$("#stat-display").text("Free-Throw: " + ((+team1data.FT)*100) + "%");
-    		console.log(eventCount+" Free-Throw: " + ((+team1data.FT)*100) + "%");
+    		ftFlag1 = true;
+	    	$("#stat-team-name").text(team1data.Team);
+	    	$("#stat-display").text("Free-Throw: " + ((+team1data.FT)*100) + "%");
+   		})
+   		.mouseleave(function() {
+   			ftFlag1 = false;
    		});
+
+
 
     // Team 2 hover functions
     $("#team-two-three")
     	.mouseover(function() {
-    		eventCount++;
-       		$("#stat-team-name").text(team2data.Team);
-    		$("#stat-display").text("Three-Point: " + ((+team2data.threeP)*100) + "%");
-    		console.log(eventCount+" Three-Point: " + ((+team2data.threeP)*100) + "%");
-    	});
+    		if( !twoFlag2 ){
+	       		$("#stat-team-name").text(team2data.Team);
+	    		$("#stat-display").text("Three-Point: " + ((+team2data.threeP)*100) + "%");
+	    	}
+    	})
+    	.mouseleave(function() {
+   			// Back to default
+   			$("#stat-team-name").text("Hover over an element");
+   			$("#stat-display").text("to see a stat");
+   		});
 
     $("#team-two-two")
     	.mouseover(function() {
-    		eventCount++;
-    		$("#stat-team-name").text(team2data.Team);
-    		$("#stat-display").text("Two-Point: " + ((+team2data.twoP)*100) + "%");
-    		console.log(eventCount+" Two-Point: " + ((+team2data.twoP)*100) + "%");
-    	});
+    		if( !ftFlag2 ){
+    			twoFlag2 = true;
+	    		$("#stat-team-name").text(team2data.Team);
+	    		$("#stat-display").text("Two-Point: " + ((+team2data.twoP)*100) + "%");
+	    	}
+    	})
+    	.mouseleave(function() {
+   			twoFlag2 = false;
+   		});
+
     $("#team-two-ft")
     	.mouseover(function() {
-    		eventCount++;
+    		ftFlag2 = true;
 	    	$("#stat-team-name").text(team2data.Team);
 	    	$("#stat-display").text("Free-Throw: " + ((+team2data.FT)*100) + "%");
-	    	console.log(eventCount+" Free-Throw: " + ((+team2data.FT)*100) + "%");
-    	});	
+    	})
+    	.mouseleave(function() {
+   			ftFlag2 = false;
+   		});	
 
 });
